4840983b9d66df900c8b02a0364a8c3ba7da7ddc
Alex <alex.kreinis@walla.co.il>
2019-09-05 16:58:21 +0300

    Added login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar.js';
 import Landing from './components/layout/Landing.js';
 import Login from './components/auth/Login.js';
 import Register from './components/auth/Register.js';
+import Alert from './components/layout/Alert.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" render={() => <Landing />} />
-        <section className="container">
-          <Switch>
-            <Route exact path="/register" render={() => <Register />} />
-            <Route exact path="/login" render={() => <Login />} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" render={() => <Landing />} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" render={() => <Register />} />
+              <Route exact path="/login" render={() => <Login />} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
